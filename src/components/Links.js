bebcb6816b647b2175fa5fe06b53d20f2d466064
pc-sam <samurai.h@yahoo.com>
2024-03-19 14:14:46 +0800

    add the name title for each div of the link

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,8 +4,8 @@ function Links({github,linkedin}) {
     return (
         <div id="links">
             <h3>Links</h3>
-            <a href={linkedin}>{linkedin}</a>
-            <a href={github}>{github}</a>
+            <div> linkedin: <a href={linkedin}>{linkedin}</a></div>
+            github: <a href={github}>{github}</a>
 
         </div>
         
